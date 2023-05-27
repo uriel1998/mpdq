eb700122954df40e004220624832e08be6dbcc06
Steven Saus <steven@stevesaus.com>
2023-05-27 13:20:08 -0400

    adding config builder as separate for the moment

---
+++ build_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# To use YAD/zenity to create config file easily.
+# FLOW
+# 
+# 1 - Set default value (with explanation)
+# 2 - Set top level genres (for those that exist in MPD)
+# 3 - Set subgenres (for those that exist in MPD)
+# 4 - Save whole shebang
